8ff632c6d9f46c9050745a6224d2fe353e6afeb6
gurdian0614 <ivan.gurdian@outlook.com>
2025-06-04 18:15:47 -0600

    Add project files.

---
+++ CRUDMVVMQ22025/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDMVVMQ22025
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CRUDMVVMQ22025/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDMVVMQ22025
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
